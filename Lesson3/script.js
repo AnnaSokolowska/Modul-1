a345740575565a19a1a9bb7c891c500c373c4bc2
Anna Sokolowska <milanka.anka@gmail.com>
2022-06-27 16:28:59 +0200

    new lesson

---
+++ Lesson3/script.js
@@ -0,0 +1,20 @@
+'use srtict';
+const productName = 'Картофель';
+const productAmount = 5;
+const productCategory = 'Овощи';
+const productPrice = 20;
+
+const productSum = productAmount * productPrice;
+
+console.log(productName);
+console.log(productSum); 
+
+const productName2 = 'Носки';
+const productAmount2 = 10;
+const productCategory2 = 'Чулочно-носочные изделия';
+const productPrice2 = 23;
+
+const productSum2 = productAmount2 * productPrice2;
+
+console.log(productName2);
+console.log(productSum2);
