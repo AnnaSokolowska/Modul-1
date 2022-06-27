1bdad62a80180c20c7db15a8017cff0a857a312a
Anna Sokolowska <milanka.anka@gmail.com>
2022-06-27 12:31:46 +0200

    новый товар

--- Lesson1/script.js
+++ Lesson1/script.js
@@ -8,3 +8,13 @@ const productSum = productAmount * productPrice;
 
 console.log(productName);
 console.log(productSum); 
+
+const productName2 = 'Носки';
+const productAmount2 = 10;
+const productCategory2 = 'Чулочно-носочные изделия';
+const productPrice2 = 23;
+
+const productSum2 = productAmount2 * productPrice2;
+
+console.log(productName2);
+console.log(productSum2);
