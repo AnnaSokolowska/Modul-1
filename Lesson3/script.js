45599f8d76e6f1363de7af74d2648c177bb13f8b
Anna Sokolowska <milanka.anka@gmail.com>
2022-06-27 18:08:55 +0200

    new task

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -7,7 +7,7 @@ const productPrice = 20;
 const productSum = productAmount * productPrice;
 
 console.log(productName);
-console.log(productSum); 
+console.log(productSum);
 
 const productName2 = 'Носки';
 const productAmount2 = 10;
@@ -18,3 +18,15 @@ const productSum2 = productAmount2 * productPrice2;
 
 console.log(productName2);
 console.log(productSum2);
+
+const newProductName = prompt('Введите наименование товара');
+const newProductCategory = prompt('Введите категорию товара');
+const newProductAmount = prompt('Введите количество товара');
+const newProductPrice = prompt('Введите цену товара');
+
+console.log(`ProductName:  ${typeof newProductName}`);
+console.log(`ProductCategory:  ${typeof newProductCategory}`);
+console.log(`ProductAmount:  ${typeof newProductAmount}`);
+console.log(`ProductPrice:  ${typeof newProductPrice}`);
+
+console.log(`На складе ${Number(newProductAmount)} единицы товара "${newProductName}" на сумму  ${(Number(newProductAmount)) * Number(newProductPrice)} рубля`);
