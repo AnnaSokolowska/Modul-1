d2a36f3866fec82acaaf96872c5afa96a565c42d
Anna Sokolowska <milanka.anka@gmail.com>
2022-06-27 20:34:24 +0200

    Lesson3 new

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -1,32 +1,42 @@
 'use srtict';
-const productName = 'Картофель';
-const productAmount = 5;
-const productCategory = 'Овощи';
-const productPrice = 20;
+{
+    const productName = 'Картофель';
+    const productAmount = 5;
+    const productCategory = 'Овощи';
+    const productPrice = 20;
 
-const productSum = productAmount * productPrice;
+    const productSum = productAmount * productPrice;
 
-console.log(productName);
-console.log(productSum);
+    console.log(productName);
+    console.log(productSum);
 
-const productName2 = 'Носки';
-const productAmount2 = 10;
-const productCategory2 = 'Чулочно-носочные изделия';
-const productPrice2 = 23;
+}
 
-const productSum2 = productAmount2 * productPrice2;
+{
+    const productName = 'Носки';
+    const productAmount = 10;
+    const productCategory = 'Чулочно-носочные изделия';
+    const productPrice = 23;
 
-console.log(productName2);
-console.log(productSum2);
+    const productSum = productAmount * productPrice;
 
-const newProductName = prompt('Введите наименование товара');
-const newProductCategory = prompt('Введите категорию товара');
-const newProductAmount = prompt('Введите количество товара');
-const newProductPrice = prompt('Введите цену товара');
+    console.log(productName);
+    console.log(productSum);
+}
 
-console.log(`ProductName:  ${typeof newProductName}`);
-console.log(`ProductCategory:  ${typeof newProductCategory}`);
-console.log(`ProductAmount:  ${typeof newProductAmount}`);
-console.log(`ProductPrice:  ${typeof newProductPrice}`);
+{
+    const productName = prompt('Введите наименование товара');
+    const productCategory = prompt('Введите категорию товара');
+    const productAmount = prompt('Введите количество товара');
+    const productPrice = prompt('Введите цену товара');
 
-console.log(`На складе ${Number(newProductAmount)} единицы товара "${newProductName}" на сумму  ${(Number(newProductAmount)) * Number(newProductPrice)} рубля`);
+    console.log(`ProductName:  ${typeof productName}`);
+    console.log(`ProductCategory:  ${typeof productCategory}`);
+    console.log(`ProductAmount:  ${typeof productAmount}`);
+    console.log(`ProductPrice:  ${typeof productPrice}`);
+
+
+    console.log(`На складе ${Number(productAmount)} единицы товара "${productName}" на сумму  ${(Number(productAmount)) * Number(productPrice)} рубля`);
+
+
+}
