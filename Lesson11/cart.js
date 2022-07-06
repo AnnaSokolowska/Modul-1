184fec9a218643e6f432b7f90954a120ae4b123d
Anna Sokolowska <milanka.anka@gmail.com>
2022-07-06 21:52:38 +0300

    Lesson11

---
+++ Lesson11/cart.js
@@ -0,0 +1,60 @@
+'use strict';
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+
+  add(productName, productAmount, productPrice) {
+    const product = {
+      name: productName,
+      amount: productAmount,
+      price: productPrice,
+    };
+    this.items.push(product);
+    this.increaseCount();
+    return this.items;
+  },
+  increaseCount() {
+    this.count = Number(this.count) + 1;
+  },
+  calculateItemPrice() {
+    let sum = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      const productSum = this.items[i].price * this.items[i].amount;
+      sum += productSum;
+    }
+    return sum;
+  },
+  // eslint-disable-next-line no-dupe-keys
+  get totalPrice() {
+    return `${this.calculateItemPrice()}`;
+  },
+  clear() {
+    this.items = [];
+    this.count = 0;
+    this.totalPrice = 0;
+  },
+
+  print() {
+    const itemsStr = JSON.stringify(this.items);
+    return `${itemsStr} \n Общая стоимость корзины: ${this.totalPrice}`;
+  },
+};
+const result = cart.add(
+  prompt('Введите наименование товара'),
+  Number(prompt('Введите количество товара')),
+  Number(prompt('Введите цену товара')),
+);
+const result1 = cart.add(
+  prompt('Введите наименование товара'),
+  Number(prompt('Введите количество товара')),
+  Number(prompt('Введите цену товара')),
+);
+
+
+console.log(result);
+console.log(result1);
+console.log(cart.print());
+console.log(cart);
+cart.clear();
+console.log(cart);
